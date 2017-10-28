57235b895ea4a11082607d436593c0a8676c13b9
Rintel <ced.laier@gmail.com>
2017-10-28 15:21:20 +0200

    added server requests

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,45 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
 import PropTypes from 'prop-types';
 
 const propTypes = {
   title: PropTypes.string.isRequired
 };
 
-const Header = ({ title }) => {
-  return (
-    <AppBar
-      className='Header'
-      title={title}
-      iconClassNameRight='muidocs-icon-navigation-expand-more'
-      showMenuIconButton={false}
-    />
-  );
-};
 
-Header.propTypes = propTypes;
+export default class Header extends React.Component {
 
-export default Header;
+  static propTypes = propTypes;
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false
+    };
+  }
+
+  handleToggle = () => this.setState({open: !this.state.open});
+
+  render() {
+    return (
+      <div>
+        <AppBar
+          className='Header'
+          title={this.props.title}
+          iconClassNameRight='muidocs-icon-navigation-expand-more'
+          onLeftIconButtonTouchTap={this.handleToggle}
+        />
+        <Drawer open={this.state.open}>
+          <MenuItem
+            onClick={this.handleToggle}
+          >
+            Close Menu
+          </MenuItem>
+          <MenuItem>Menu Item 2</MenuItem>
+        </Drawer>
+      </div>
+    );
+  }
+}
---
+++ src/pages/EventDetails/EventDetails.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../../components/Header/Header';
+
+const EventDetails = () => {
+  return (
+      <div className='content-container'>
+        <Header
+          title={'Breminale-Web'}
+        />
+      <img src={"https://via.placeholder.com/350x150"} />
+      </div>
+    );
+};
+
+export default EventDetails;
